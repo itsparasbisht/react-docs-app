da3d1df251cfb8822a250c3315a61ba0b8eef2ee
itsparasbisht <iamparasbisht@gmail.com>
2023-07-02 20:47:27 +0530

    setting toolbar options

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -3,6 +3,18 @@ import "quill/dist/quill.snow.css";
 import { useRef } from "react";
 import { useEffect } from "react";
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: {} }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
 export default function TexEditor() {
   const containerRef = useRef();
 
@@ -11,6 +23,9 @@ export default function TexEditor() {
     containerRef.current.append(editor);
     new Quill(editor, {
       theme: "snow",
+      modules: {
+        toolbar: TOOLBAR_OPTIONS,
+      },
     });
 
     return () => {
