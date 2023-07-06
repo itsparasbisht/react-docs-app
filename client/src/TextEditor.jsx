71597007d53f26b6953d9b7608c079e84daa62ae
itsparasbisht <iamparasbisht@gmail.com>
2023-07-06 20:52:18 +0530

    fixes

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,13 +1,11 @@
+import { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-import { useState } from "react";
-import { useRef } from "react";
-import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  [{ font: {} }],
+  [{ font: [] }],
   [{ list: "ordered" }, { list: "bullet" }],
   ["bold", "italic", "underline"],
   [{ color: [] }, { background: [] }],
@@ -17,10 +15,9 @@ const TOOLBAR_OPTIONS = [
   ["clean"],
 ];
 
-export default function TexEditor() {
+export default function TextEditor() {
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
-  const containerRef = useRef();
 
   useEffect(() => {
     const s = io("http://localhost:5000");
@@ -32,38 +29,30 @@ export default function TexEditor() {
   }, []);
 
   useEffect(() => {
-    if (socket == undefined || quill == undefined) return;
+    if (socket == null || quill == null) return;
 
     const handler = (delta, oldDelta, source) => {
       if (source !== "user") return;
-      console.log("opop");
       socket.emit("send-changes", delta);
     };
-
-    quill.on("text-change", () => {
-      console.log("hello");
-    });
+    quill.on("text-change", handler);
 
     return () => {
       quill.off("text-change", handler);
     };
   }, [socket, quill]);
 
-  useEffect(() => {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = "";
     const editor = document.createElement("div");
-    containerRef.current.append(editor);
+    wrapper.append(editor);
     const q = new Quill(editor, {
       theme: "snow",
-      modules: {
-        toolbar: TOOLBAR_OPTIONS,
-      },
+      modules: { toolbar: TOOLBAR_OPTIONS },
     });
     setQuill(q);
-
-    return () => {
-      containerRef.current = document.createElement("div");
-    };
   }, []);
-
-  return <div className="container" ref={containerRef}></div>;
+  return <div className="container" ref={wrapperRef}></div>;
 }
