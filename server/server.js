078b90a79e2457742d8186dba11ca53d2f3ed927
itsparasbisht <iamparasbisht@gmail.com>
2023-07-08 20:32:58 +0530

    broadcast changes and update the editor

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ io.on("connection", (socket) => {
   console.log("connected...");
 
   socket.on("send-changes", (delta) => {
-    console.log(delta);
+    socket.broadcast.emit("receive-changes", delta);
   });
 });
