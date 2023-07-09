8c41dd933d0165b241562341fc8c8a4f40bd8b92
itsparasbisht <iamparasbisht@gmail.com>
2023-07-09 12:13:37 +0530

    syn changes in mongoDB

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_URI);
+
 const io = require("socket.io")(5000, {
   cors: {
     origin: "http://localhost:5173",
@@ -5,17 +11,31 @@ const io = require("socket.io")(5000, {
   },
 });
 
+const defaultValue = "";
+
 io.on("connection", (socket) => {
   console.log("connected...");
 
-  socket.on("get-document", (documentId) => {
-    const data = "";
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
     socket.join(documentId);
 
-    socket.emit("load-document", data);
+    socket.emit("load-document", document.data);
 
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
   });
 });
+
+async function findOrCreateDocument(id) {
+  if (id === null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return Document.create({ _id: id, data: defaultValue });
+}
