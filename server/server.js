d9ed6b711a08926c1275e7182f29d95435dbdd1f
itsparasbisht <iamparasbisht@gmail.com>
2023-07-09 11:50:55 +0530

    socket.join()

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ const io = require("socket.io")(5000, {
 io.on("connection", (socket) => {
   console.log("connected...");
 
-  socket.on("send-changes", (delta) => {
-    socket.broadcast.emit("receive-changes", delta);
+  socket.on("get-document", (documentId) => {
+    const data = "";
+    socket.join(documentId);
+
+    socket.emit("load-document", data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
   });
 });
